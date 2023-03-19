11bb0c5c23e28c3d3b29300562a731de76c40950
kichinda <53686544+kichinda@users.noreply.github.com>
2023-03-19 13:25:43 +0300

    done

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -1,23 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Plant, type: :model do
-  let!(:plant) { Plant.new(name: "Aloe", image: "./images/aloe.jpg", price: 15.99) }
-
-  describe '#name' do
-    it 'returns the plant\'s name' do
-      expect(plant.name).to eq('Aloe')
+  describe "validations" do
+    it "is invalid without a name" do
+      plant = Plant.new(name: nil)
+      expect(plant).to be_invalid
     end
-  end
-
-  describe '#image' do
-    it 'returns the plant\'s last name' do
-      expect(plant.image).to eq('./images/aloe.jpg')
-    end
-  end
-
-  describe '#price' do
-    it 'returns the plant\'s price' do
-      expect(plant.price).to eq(15.99)
+    
+    it "is valid with a name" do
+      plant = Plant.new(name: "fern")
+      expect(plant).to be_valid
     end
   end
 end
